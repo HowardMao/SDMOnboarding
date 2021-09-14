b8ea6809d96354657e9fc40856ab61e0523f1eb7
Howard Mao <howardmao333@gmail.com>
2021-09-14 13:44:02 +1200

    added path.join into root index.js

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 app.get('/', function(req, res) {
     res.send('hello world');
