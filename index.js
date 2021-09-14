fa03b248a944ff3051356a4341331d997308b2bb
Howard Mao <howardmao333@gmail.com>
2021-09-14 13:56:14 +1200

    added express.static to index.js

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
