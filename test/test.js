02981726b2070ec28605548c0cc5f9fb317fcc47
Howard Mao <howardmao333@gmail.com>
2021-09-21 12:50:49 +1200

    added new modules

--- test/test.js
+++ test/test.js
@@ -1,19 +1,36 @@
-const chai = require('chai');
+//Backend Tests
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../index');
+
+chai.use(chaiHttp);
+chai.should();
 
 class Hello{
-    Hello(){
-    }
+    Hello(){}
 
     SayHello(){
         return "hello";
     }
 }
 
-describe("Testing the test", function() {
+describe("Testing the test in server", function() {
     it('should return \'hello\'', function(){
         var hello = new Hello();
         var word = hello.SayHello();
 
         chai.expect(word).to.equal("hello");
+    })
+})
+
+describe("Testing getting goals", () =>{
+    it('should GET all goals', (done) =>{
+        chai.request(server)
+            .get('/api/goals')
+            .end((err, res) =>{
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                done();
+            })
     })
 })
