76cc96512f345c103b012cf203596ce420bd03ba
Thilan Shiroyuki Liyanage <Shiroyuki-Liyanage@users.noreply.github.com>
2021-09-22 12:21:23 +1200

    Added TDD, Selecting Goals

--- client/src/components/Goal.js
+++ client/src/components/Goal.js
@@ -1,47 +1,56 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Input from './Input';
-import ListGoal from './ListGoal';
+import React, { Component } from "react";
+import axios from "axios";
+import Input from "./Input";
+import ListGoal from "./ListGoal";
 
-class Goal extends Component{
-    state = {
-        goals: []
-    };
+class Goal extends Component {
+  state = {
+    goals: [],
+    myGoals: [],
+  };
 
-    componentDidMount(){
-        this.getGoals();
-    }
+  componentDidMount() {
+    this.getGoals();
+  }
 
-    getGoals = () => {
-        axios.get('/api/goals')
-            .then((res) => {
-                if(res.data) {
-                    this.setState({ goals: res.data})
-                }})
-            .catch((err) => console.log(err));
-    };
+  getGoals = () => {
+    axios
+      .get("/api/goals")
+      .then((res) => {
+        if (res.data) {
+          this.setState({ goals: res.data });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    // deleteGoals = (id) => {
-    //     axios.delete(`/api/goals/${id}`)
-    //         .then((res) => {
-    //             if(res.data){
-    //                 this.getGoals();
-    //             }
-    //         })
-    //         .catch((err) => console.log(err));
-    // };
+  // deleteGoals = (id) => {
+  //     axios.delete(`/api/goals/${id}`)
+  //         .then((res) => {
+  //             if(res.data){
+  //                 this.getGoals();
+  //             }
+  //         })
+  //         .catch((err) => console.log(err));
+  // };
 
-    render() {
-        let {goals} = this.state;
+  render() {
+    let { goals, myGoals } = this.state;
 
-        return(
-            <div>
-                <h1>My Goals</h1>
-                <Input getGoals={this.getGoals}/>
-                <ListGoal goals={goals} deleteGoals={this.deleteGoals}/>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <h1>My Goals</h1>
+        <Input getGoals={this.getGoals} />
+        <ListGoal
+          goals={goals}
+          myGoals={myGoals}
+          deleteGoals={this.deleteGoals}
+        />
+      </div>
+    );
+  }
 }
 
 export default Goal;
--- client/src/components/ListGoal.js
+++ client/src/components/ListGoal.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React from "react";
 
-const ListGoal = ({goals}) => {
-    return (
-        <ul>
-            {goals && goals.length > 0 ? (
-                goals.map((goal) => {
-                    return (
-                        <li key={goal._id}>
-                            {goal.goal}
-                        </li>
-                    );
-                })
-            ) : (
-                <li>No Goals left</li>
-            )}
-        </ul>
-    );
+const ListGoal = ({ goals, myGoals }) => {
+  return (
+    <div>
+      <ul>
+        {goals && goals.length > 0 ? (
+          goals.map((goal) => {
+            return (
+              <li key={goal._id} onClick={() => ClickGoal(goal, myGoals)}>
+                {goal.goal}
+              </li>
+            );
+          })
+        ) : (
+          <li>No Goals left</li>
+        )}
+      </ul>
+      <ul>
+        {myGoals && myGoals.length > 0 ? (
+          myGoals.map((goal) => {
+            return (
+              <li key={goal._id} onClick={() => ClickMyGoal(goal, myGoals)}>
+                {goal.goal}
+              </li>
+            );
+          })
+        ) : (
+          <li>No Goals left</li>
+        )}
+      </ul>
+    </div>
+  );
 };
+
+function ClickGoal(goal, myGoals) {
+  myGoals.push(goal);
+  console.log(goal);
+  console.log(myGoals);
+}
+
+function ClickMyGoal(goal, myGoals) {
+  myGoals.push(goal);
+}
 
 export default ListGoal;
